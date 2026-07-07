985014761f4ec8e267f5e999381f892f24a0cc79
Fiona <fmacgarvey@gmail.com>
2026-07-07 13:39:07 -0400

    add word_cloud_generator.py

---
+++ word_cloud_generator.py
@@ -0,0 +1,49 @@
+import re
+import nltk
+nltk.download('stopwords')
+nltk.download('punkt_tab')
+
+from wordcloud import WordCloud
+import matplotlib.pyplot as plt
+
+
+'''Processing Steps:
+- clean
+- tokenize
+- remove stopwords
+'''
+
+stopwords = nltk.corpus.stopwords.words('english')
+
+
+
+def process_speech(text):
+   #Clean text by removing punctuation and numbers
+   text=text.lower()
+   text = re.sub(r'\W', ' ', text)
+   text = re.sub(r'\d', ' ', text)
+   
+   #Tokenize text
+   speech_tokens = nltk.tokenize.word_tokenize(text)
+
+   #Remove stopwords
+   stopword_set=set(nltk.corpus.stopwords.words('english'))
+
+   words = [word for word in speech_tokens if word not in stopword_set]
+
+   return ' '.join(words)
+
+def main():
+   with open('address_to_the_nation_december_17.txt', 'r') as file:
+      raw_speech=file.read()
+
+   speech_string = process_speech(raw_speech)
+
+   wc = WordCloud().generate(''.join(speech_string))
+   plt.imshow(wc)
+   plt.show()
+
+if __name__ == "__main__":
+   main()
+
+
